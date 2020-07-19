dde412c1cb0bd00928d5ac62dd61673c736c173a
Hasnatul Jelan <bsse1026@iit.du.ac.bd>
2020-07-19 16:35:42 +0600

    Update eScripts.js

--- PHP_a0/eScripts.js
+++ PHP_a0/eScripts.js
@@ -2,13 +2,20 @@ function check() {
     var p= document.getElementsByName("u_pass")[0];
     var pp= document.getElementsByName("re-pass")[0];
 
-    if (pp.value!=='' && pp.value===p.value) {
-        document.getElementById('message').style.color = 'green';
-        document.getElementById('message').innerHTML = 'Password matched';
-        document.getElementById("sb").disabled =false;
-    } else {
+    if(p.value.length > pp.value.length && p.value.includes(pp.value) && p.value[0]===pp.value[0]){
         document.getElementById('message').style.color = 'grey';
+        document.getElementById('message').innerHTML = 'Good so far';
+        document.getElementById("sb").disabled = true;
+    }
+    else{
+        document.getElementById('message').style.color = 'red';
         document.getElementById('message').innerHTML = 'Password is not matching';
         document.getElementById("sb").disabled =true;
     }
+    if (pp.value===p.value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'Password Matched';
+        document.getElementById("sb").disabled =false;
+    }
+    if(pp.value.length===0){  document.getElementById('message').innerHTML = '';}
 }
